b7ae6097ec7abd9b8cc980b16c2a92ebac09c8a5
Mulviill <mulvihill.chris@outlook.com>
2024-12-02 18:58:23 -0700

    building out file manipulation logic

--- internal/loadConfig.go
+++ internal/loadConfig.go
@@ -1,13 +1,23 @@
 /*
-Loading the Configuration file and setting default vaules for options not defined
+Loading the Configuration file and setting default values for options not defined
 in the config.
 */
 package internal
+
+import "os"
 
 type Config struct {
 	DestinationPath string `json:"DestinationPath"`
 }
 
+func (c *Config) SetDefaults() {
+	if c.DestinationPath == "" {
+		userHome := os.Getenv("HOME")
+		c.DestinationPath = userHome + "/json2go-gen"
+	}
+}
+
 func LoadConfig(conf *Config) {
+	
 	conf.DestinationPath = "test"
 }
